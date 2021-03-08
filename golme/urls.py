1abb94900042777cc2f478f492be8a780807e554
Tiago Cordeiro <tiago@mulhergorila.com>
2021-03-08 18:19:45 -0300

    hotfix returning views, urls and facades

--- golme/urls.py
+++ golme/urls.py
@@ -24,4 +24,5 @@ admin.site.site_header = 'LME'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
