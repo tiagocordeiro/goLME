10e7d3a306a9b74131bb0f90f047d4def3afda5e
Tiago Cordeiro <tiago@mulhergorila.com>
2021-05-13 11:24:12 -0300

    Update dependencies
    - Atualiza dependencias
    - Upgrading to Django > 3.2 and
      fixing DEFAULT_AUTO_FIELD warnings
    - Make Migrations
    - Update python runtime
    
    This close #15

--- golme/settings.py
+++ golme/settings.py
@@ -88,6 +88,8 @@ DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
+# Required by Django > 3.2
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
