8e160b32bdcce2d0309f380bfbf5d00ab508ad5c
Tiago Cordeiro ⚡ <tiago@mulhergorila.com>
2025-05-02 08:21:37 -0300

    Update storage settings to comply with new Django requirements
    Replaced deprecated storage configuration with the new STORAGES setting as per Django 4.2 documentation. Added Cloudinary credentials for proper integration and ensured secure storage configuration.

--- golme/settings.py
+++ golme/settings.py
@@ -160,3 +160,18 @@ if CLOUDINARY_URL:  # pragma: no cover
 
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
     STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
+    # Storages
+
+    # Default STORAGES from Django documentation
+    # See: https://docs.djangoproject.com/en/4.2/ref/settings/#std-setting-STORAGES
+    STORAGES = {
+        "default": {"BACKEND": "cloudinary_storage.storage.RawMediaCloudinaryStorage"},
+        "staticfiles": {"BACKEND": "cloudinary_storage.storage.StaticCloudinaryStorage"},
+    }
+
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+        'API_KEY': config('CLOUDINARY_API_KEY'),
+        'API_SECRET': config('CLOUDINARY_API_SECRET'),
+        'SECURE': True,
+    }
